ceb8eb3741cf3a8d6b0e471f38546fc67caa2255
Sebastien Vehaegen <sebastien.vehaegen@outlook.com>
2025-04-17 16:33:05 +0200

    Create Rotation

---
+++ Runtime/QuickRotationMono.cs
@@ -0,0 +1,24 @@
+using Oculus.Interaction.Editor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Sebastien.QuickScript
+{
+    public class QuickRotationMono : MonoBehaviour
+    {
+        public Transform m_whatToRotation;
+        public float m_globalSpeedRotation = 360;
+        public Vector3 m_rotationAxis = Vector3.up;
+        public Space m_space = Space.Self;
+
+        // Update is called once per frame
+        void Update()
+        {
+            float delaTime = Time.deltaTime;
+            float result = delaTime * m_globalSpeedRotation;
+            m_whatToRotation.Rotate(m_rotationAxis, result, m_space);
+        }
+    }
+}
+
+ 
